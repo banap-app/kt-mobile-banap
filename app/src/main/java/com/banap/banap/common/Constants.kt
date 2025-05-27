41d7ecbd68e1c611fb7c45356cfb8c73d49e9e9d
gilSFranco <gilmarsoaresfranco@gmail.com>
2025-05-27 08:52:51 -0300

    feat: change constants

--- app/src/main/java/com/banap/banap/common/Constants.kt
+++ app/src/main/java/com/banap/banap/common/Constants.kt
@@ -1,8 +1,8 @@
 package com.banap.banap.common
 
 object Constants {
-    const val AUTH_URL = "http://10.0.2.2:8192/"
-    const val PRODUCER_URL = "http://10.0.2.2:3000/"
+    const val AUTH_URL = "http://auth.banap.com.br:8192/"
+    const val PRODUCER_URL = "http://producer.banap.com.br:3000/"
     const val WEATHER_URL = "https://api.openweathermap.org/data/2.5/"
 }
 
